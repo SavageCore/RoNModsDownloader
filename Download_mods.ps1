e5458f3bb48e7aac59737c11e14cb03feac48cf5
SavageCore <talk@savagecore.uk>
2023-12-19 17:48:30 +0000

    Satisfy PSScriptAnalyzer

--- Download_mods.ps1
+++ Download_mods.ps1
@@ -1,5 +1,5 @@
 # Used to simplify y/n prompts further in the script
-function User-Confirm
+function Confirm-User
 {
 	param ([string]$msg)
 	do
@@ -11,7 +11,7 @@ function User-Confirm
 		}
 		elseif ($yn -ne 'y')
 		{
-			Echo "Enter y fer yes or n for no"
+			Write-Output "Enter y fer yes or n for no"
 		}
 	}
 	while($yn -ne "y")
@@ -20,7 +20,7 @@ function User-Confirm
 
 if (Test-Path config.json)
 {
-	Echo "Reading settings from config.json."
+	Write-Output "Reading settings from config.json."
 	$config=Get-Content config.json | ConvertFrom-Json
 }
 else
@@ -28,9 +28,9 @@ else
 	$config=@{}
 
 	$pcvr=$false
-	Echo "No configuration file found. Let's set up a new one."
-	Echo "Are you running BONELAB on Windows / Steam / PCVR? Type 'y' for yes or 'n' for no."
-	if (User-Confirm "Download mods for Windows?")
+	Write-Output "No configuration file found. Let's set up a new one."
+	Write-Output "Are you running BONELAB on Windows / Steam / PCVR? Type 'y' for yes or 'n' for no."
+	if (Confirm-User "Download mods for Windows?")
 	{
 		$platf="windows"
 		$dest="$env:appdata\..\LocalLow\Stress Level Zero\BONELAB\Mods"
@@ -38,33 +38,33 @@ else
 	}
 	else
 	{
-		Echo "Are you running BONELAB on Quest 2?"
-		if (User-Confirm "Download mods for android/quest?")
+		Write-Output "Are you running BONELAB on Quest 2?"
+		if (Confirm-User "Download mods for android/quest?")
 		{
 			$platf="android"
 			$dest=(Get-Location).Path+"\mods"
 		}
 	}
-	if($platf -eq $null)
+	if($null -eq $platf)
 	{
-		Echo "Sorry, there are no other supported platforms :("
+		Write-Output "Sorry, there are no other supported platforms :("
 		Pause
 		Exit
 	}
 
-	$unpack=User-Confirm "Do you wish to automatically unpack or install downloaded/updated mods?"
+	$unpack=Confirm-User "Do you wish to automatically unpack or install downloaded/updated mods?"
 
 	if($unpack)
 	{
-		Echo "The default installation path is: $dest"
+		Write-Output "The default installation path is: $dest"
 		if($pcvr)
 		{
-			Echo "(this is the default location for your BONELAB mod folder)"
+			Write-Output "(this is the default location for your BONELAB mod folder)"
 		}
 
-		if(-not(User-Confirm "Install/unpack mods to that location?"))
+		if(-not(Confirm-User "Install/unpack mods to that location?"))
 		{
-			Echo "Enter the path to the desired location."
+			Write-Output "Enter the path to the desired location."
 			$dest=Read-Host "Desired location"
 		}
 		if(-not(Test-Path $dest))
@@ -73,19 +73,19 @@ else
 		}
 	}
 
-	Echo "Enter an OAuth token with read access."
-	Echo "Check the readme if you don't know how to set this up".
+	Write-Output "Enter an OAuth token with read access."
+	Write-Output "Check the readme if you don't know how to set this up".
 	do
 	{
 		$tok=Read-Host "OAuth token"
 		if($tok.length -lt 1000)
 		{
-			Echo "This doesn't seem to be a valid OAuth token. Make sure to copy an OAuth token, NOT an API access key"
-			Echo "(a proper OAuth token should be much longer than the value you entered)"
+			Write-Output "This doesn't seem to be a valid OAuth token. Make sure to copy an OAuth token, NOT an API access key"
+			Write-Output "(a proper OAuth token should be much longer than the value you entered)"
 		}
 	}
 	while($tok.length -lt 1000)
-	Echo "Thank you, that should be everything I need."
+	Write-Output "Thank you, that should be everything I need."
 
 	$config.token=$tok
 	$config.platform=$platf
@@ -96,8 +96,8 @@ else
 	# (so that the user doesn't have to go trough the setup again if the script doesn't run completely)
 	$config | ConvertTo-Json | Set-Content config.json
 
-	Echo "I'll now start downloading all your subscribed mods."
-	Echo '' '' '' ''
+	Write-Output "I'll now start downloading all your subscribed mods."
+	Write-Output '' '' '' ''
 }
 
 $token=$config.token
@@ -109,19 +109,19 @@ if (-not(Test-Path zips))
 	Mkdir zips
 }
 
-Echo "Checking subscriptions..."
+Write-Output "Checking subscriptions..."
 $sublist_json=Invoke-WebRequest -UseBasicParsing -URI https://api.mod.io/v1/me/subscribed?game_id=3809 -Method GET -Headers @{"Authorization"="Bearer ${token}";"Accept"="application/json"}
 $sublist=ConvertFrom-Json $sublist_json.Content
 
 $len=$sublist.data.length
 [string]$len_str=$len
-Echo "Found $len_str subscription(s)."
+Write-Output "Found $len_str subscription(s)."
 
 for ($i = 0; $i -lt $len; $i++)
 {
 	$sub=$sublist.data[$i]
 	$subname=$sub.name
-	Echo "Requesting info about subscription $subname..."
+	Write-Output "Requesting info about subscription $subname..."
 	[string]$modid=$sub.id
 	$mod_json=Invoke-WebRequest -UseBasicParsing -URI https://api.mod.io/v1/games/3809/mods/${modid}/files -Method GET -Headers @{"Authorization"="Bearer ${token}";"Accept"="application/json"}
 	$mod=ConvertFrom-Json $mod_json.Content
@@ -132,28 +132,28 @@ for ($i = 0; $i -lt $len; $i++)
 
 	# get latest version in remaining files and keep only files matching that version
 	$mod.data=@($mod.data)
-	$lastver=($mod.data | Select-Object -ExpandProperty version | Measure -Maximum).Maximum
+	$lastver=($mod.data | Select-Object -ExpandProperty version | Measure-Object -Maximum).Maximum
 	$mod.data=$mod.data | Where-Object { $_.version -ge $lastver }
 	$mod.data=@($mod.data)
-	Echo "Latest version: $lastver"
+	Write-Output "Latest version: $lastver"
 
 	# Write data about this sub to config
 	$name=$sub.name_id
 
 	$update=$true
-	if ($config.${name} -eq $null)
+	if ($null -eq $config.${name})
 	{
 		$config | Add-Member -Name $name -Value @{} -MemberType NoteProperty
 		$config.${name}.date_updated=$sub.date_updated
 	}
 	elseif ($sub.date_updated -le $config.${name}.date_updated){
 		$update=$false #already up to date
-		Echo "$subname seems to be up to date - checking for missing files..."
+		Write-Output "$subname seems to be up to date - checking for missing files..."
 	}
 
 	$datalen=$mod.data.length
 	[string]$datalen_str=$datalen
-	Echo "$subname contains $datalen_str file(s)."
+	Write-Output "$subname contains $datalen_str file(s)."
 
 	for ($j = 0; $j -lt $datalen; $j++)
 	{
@@ -161,7 +161,7 @@ for ($i = 0; $i -lt $len; $i++)
 		$file=$data.filename
 		if (-not(Test-Path zips/$file) -or $update)
 		{
-			Echo "  Downloading $file..."
+			Write-Output "  Downloading $file..."
 			$url=$data.download.binary_url
 
 			# Suppress progress bar to vastly improve download speed
@@ -171,13 +171,13 @@ for ($i = 0; $i -lt $len; $i++)
 			Invoke-WebRequest -UseBasicParsing -URI $url -OutFile zips/$file
 			if ($unpack)
 			{
-				Echo "  Unpacking $file..."
+				Write-Output "  Unpacking $file..."
 				Expand-Archive zips/$file -DestinationPath $destination -Force
 			}
 		}
 		else
 		{
-			Echo "  Skipped $file (already exists and up to date)."
+			Write-Output "  Skipped $file (already exists and up to date)."
 		}
 	}
 }
@@ -185,6 +185,6 @@ for ($i = 0; $i -lt $len; $i++)
 # Write updates to the config file
 $config | ConvertTo-Json | Set-Content config.json
 
-Echo '' '' '' '' '' '' ''
-Echo "This seems to be everything :)"
+Write-Output '' '' '' '' '' '' ''
+Write-Output "This seems to be everything :)"
 Pause
