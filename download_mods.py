a290a460c222abfb9d62f47a9c2c2558b6f3d1c3
SavageCore <talk@savagecore.uk>
2024-08-05 21:01:32 +0100

    0.2.3

--- download_mods.py
+++ download_mods.py
@@ -15,7 +15,7 @@ from helpers.modio import get_subscriptions, update_subscriptions_config
 
 
 REPO = "SavageCore/RoNModsDownloader"
-CURRENT_VERSION = "0.2.2"
+CURRENT_VERSION = "0.2.3"
 APP_PATH = os.path.dirname(os.path.abspath(sys.executable))
 
 
