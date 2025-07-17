4d90715e69fcbc5c708b3506ecfbbac230e47004
SavageCore <oliver.sayers@squarezone.co.uk>
2025-07-17 15:09:53 +0100

    Create OAuth token with Selenium

--- helpers/config.py
+++ helpers/config.py
@@ -1,5 +1,12 @@
 import json
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 def read_config():
     """
@@ -26,7 +33,7 @@ def create_config():
     """
     oauth_token = get_oauth_token()
     if not oauth_token:
-        update_oauth_token()
+        create_oauth_token()
 
     # Create the configuration file
     config = {"subscribed_mods": []}
@@ -70,14 +77,14 @@ def get_oauth_token():
 
             # Prompt for a new OAuth token if we failed to migrate it
             if not oauth_token:
-                return update_oauth_token()
+                return create_oauth_token()
 
             return oauth_token
     except FileNotFoundError:
         # .auth file does not exist, so prompt for a new OAuth token if we fail to migrate it
         oauth_token = migrate_oauth_token()
         if not oauth_token:
-            oauth_token = update_oauth_token()
+            oauth_token = create_oauth_token()
 
         return oauth_token
 
@@ -110,9 +117,9 @@ def save_oauth_token(oauth_token):
 
 
 def update_oauth_token():
-    """
-    Prompt for and update the OAuth token in the .auth file.
-    """
+    # """
+    # Prompt for and update the OAuth token in the .auth file.
+    # """
     prompt = "Enter a new OAuth token with read access: "
     oauth_token = input(prompt)
 
@@ -121,4 +128,64 @@ def update_oauth_token():
 
     save_oauth_token(oauth_token)
 
+    return oauth_token
+
+
+def create_oauth_token():
+    """
+    Login to mod.io and create a new OAuth token.
+    Returns
+    -------
+    str
+        The OAuth token.
+    """
+    print(
+        "Getting OAuth token, you'll see a browser window open, please log in manually and wait for the token to be created.\n"
+    )
+    print("Do not worry, no password is stored, only the OAuth token is saved.\n")
+    chrome_options = Options()
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-logging"]
+    )  # Suppress driver logs
+
+    service = Service(log_path="nul")  # On Windows use "nul", on Unix use "/dev/null"
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.implicitly_wait(6)
+    driver.get("https://mod.io/login")
+    # Wait for login to complete, browser will navigate to "https://mod.io/g" once done
+    WebDriverWait(driver, 120).until(EC.url_to_be("https://mod.io/g"))
+
+    print("Login successful, automatically generating OAuth token... TOUCH NOTHING!\n")
+
+    # Once navigated, go to the access page
+    driver.get("https://mod.io/me/access")
+
+    # Find input with placeholder "New token name"
+    input_element = driver.find_element(
+        "xpath", "//input[@placeholder='New token name']"
+    )
+    # Clear the input field and enter a new token name
+    input_element.clear()
+    input_element.send_keys("RoNModsDownloader")
+
+    # Get parent element to find the "+" button, which is "up" 5 levels in the DOM
+    parent_element = input_element.find_element("xpath", "../../../../../../..")
+
+    # Click the create button
+    create_button = parent_element.find_element(By.TAG_NAME, "button")
+    create_button.click()
+
+    # Find the new token
+    token_created_span = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span[text()=' Token Created! ']"))
+    )
+    token_created_span_parent = token_created_span.find_element(By.XPATH, "..")
+    token_input = token_created_span_parent.find_element(By.XPATH, ".//input")
+    oauth_token = token_input.get_attribute("value")
+    driver.quit()
+    print("OAuth token created successfully. Continuing...")
+
+    # Save the OAuth token to the .auth file
+    save_oauth_token(oauth_token)
     return oauth_token
